3fa3e7eb61e011e3d604fb11203973304aa0acad
xxx <129951174+Leozinho33@users.noreply.github.com>
2024-05-21 10:55:40 -0300

    Create num.py

---
+++ num.py
@@ -0,0 +1,14 @@
+num1 = 5
+num2 = 5
+print ("resultado;",num1 + num2)
+n3 = "8"
+n4 = "5"
+print ("resultado:", n3 + n4)
+n5 = "8"
+n6 = "5x"
+print ("resultado :",n5 + n6)
+
+n8 = input("digite um valor")
+
+
+print ("resultado:",n8+ "5")
