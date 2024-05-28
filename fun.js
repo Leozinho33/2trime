f1be65254b3120f3e2d052e462f0075e678a76c4
unavailable <129951174+Leozinho33@users.noreply.github.com>
2024-05-28 09:45:01 -0300

    Create fun.js

---
+++ fun.js
@@ -0,0 +1,20 @@
+console.log(soma(5,9));
+function soma (v1,v2){
+    return v1 + v2;
+}
+function nomeidade (nome="",idade){
+    console.log (`nome:${nome} -idade:${idade}`); 
+} 
+nomeidade ("Leonardo","18")
+nomeidade ("22","Leonardo")
+console.log(soma(1));
+nomeidade ("Santos");
+
+
+multiplicacao(1358,86);
+multiplicacao(soma(3,5),soma(8,7));
+multiplicacao(5); 
+
+
+
+
