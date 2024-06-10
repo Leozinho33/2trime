7eb0147b99a2fb214514148679196f1b7a7ee6c1
unavailable <129951174+Leozinho33@users.noreply.github.com>
2024-06-10 12:33:49 -0300

    Create lista.js

---
+++ lista.js
@@ -0,0 +1,13 @@
+const lista = ["sabonete","carne","arroz","macarrao","leite","cafe","cha","suco","frango","maminha"]
+lista. push ("")
+lista.pop
+nlista = lista.splice (3,4)
+console.log(nlista)
+lista1 = lista.slice (0,5)
+lista2 = lista.slice (5)
+console.log (lista1)
+console.log (lista2)
+lista1.push(" ")
+lista2.push(" ")
+listacompleta = lista1.concat(lista2)
+console.log(listacompleta)
