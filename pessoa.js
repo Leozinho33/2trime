2ca0b2417d43de3134d42b2c5cb8c09baba238c2
unavailable <129951174+Leozinho33@users.noreply.github.com>
2024-07-31 08:57:11 -0300

    Create pessoa.js

---
+++ pessoa.js
@@ -0,0 +1,16 @@
+const pessoa = {
+    nome:"Monica",
+    profissao: "Pedagoga"
+}
+
+console.log (pessoa.nome)
+console.log (pessoa.telefone)
+pessoa.telefone = "41 9999-5555"
+console.log (pessoa.telefone)
+console.log (Pessoa)
+const novaPessoa = {
+    nome: 'teste'
+
+}
+
+pessoa = novaPessoa
